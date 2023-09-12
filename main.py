d622732810a0eaa4a88058aadeced8ab7edbc691
sodipe <98396909+so-dipe@users.noreply.github.com>
2023-09-12 15:35:50 +0000

    updates

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from flask import Flask, render_template, request, jsonify
 
 from resume.resume_generator import generate_custom_resume
 
+import tiktoken
+
 app = Flask(__name__)
+
+encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
 
 @app.route("/")
 def index():
@@ -12,6 +16,12 @@ def index():
 def generate_resume():
     resume_text = request.form.get("resume")
     job_description = request.form.get("job_description")
+
+    # Check the length of both texts
+    total_length = len(encoding.encode(job_description)) + len(encoding.encode(resume_text))
+
+    if total_length > 3500:
+        return jsonify({"error": "Combined length of resume and job description is too long."})
 
     # Call your function to generate custom resume
     generated_resume = generate_custom_resume(job_description, resume_text)
